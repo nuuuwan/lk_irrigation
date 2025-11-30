e93bc02548378d83ffe07357b878c713d30385fa
nuuuwan <nuuuwan@gmail.com>
2025-11-30 11:07:09 +0530

    Update test cases to require network access for remote data loading methods

--- tests/test_rwl_data.py
+++ tests/test_rwl_data.py
@@ -5,13 +5,14 @@ from lk_irrigation import RiverWaterLevelData
 
 class TestCase(unittest.TestCase):
 
-    @unittest.skip("slow")
+    @unittest.skip("Requires network access")
     def test_load_station_from_remote(self):
         d_list = RiverWaterLevelData.load_station_from_remote(
-            "Nagalagam Street"
+            "Nagalagam Street", 7, 50, 25
         )
         self.assertGreater(len(d_list), 0)
 
+    @unittest.skip("Requires network access")
     def test_load_all_from_remote(self):
-        d_list = RiverWaterLevelData.load_all_from_remote()
+        d_list = RiverWaterLevelData.load_all_from_remote(7, 50, 25)
         self.assertGreater(len(d_list), 0)
