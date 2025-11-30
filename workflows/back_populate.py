1fa6088709e8a512667c1f94add02f953b59c0ff
nuuuwan <nuuuwan@gmail.com>
2025-11-30 17:33:23 +0530

    Fix total_pages parameter in back_populate.py for correct data loading

--- workflows/back_populate.py
+++ workflows/back_populate.py
@@ -2,7 +2,7 @@ from lk_irrigation import ReadMe, RiverWaterLevelData
 
 if __name__ == "__main__":
     RiverWaterLevelData.load_all_from_remote(
-        days_offset=112, total_pages=1_000, page_size=100
+        days_offset=112, total_pages=10_000, page_size=100
     )
     RiverWaterLevelData.write_all()
     ReadMe().build()
