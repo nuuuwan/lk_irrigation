2b280b9f951defbb15d14dff8b88acc75d8ac429
nuuuwan <nuuuwan@gmail.com>
2025-11-30 12:56:21 +0530

    Add alert property to RiverWaterLevelData for flood level classification

--- src/lk_irrigation/rwld/RiverWaterLevelData.py
+++ src/lk_irrigation/rwld/RiverWaterLevelData.py
@@ -4,6 +4,7 @@ from functools import cached_property
 from utils import Log
 
 from lk_irrigation.base.HasTimeMixin import HasTimeMixin
+from lk_irrigation.core import Alert
 from lk_irrigation.core.Station import Station
 from lk_irrigation.rwld.RiverWaterLevelDataFileMixin import \
     RiverWaterLevelDataFileMixin
@@ -24,3 +25,13 @@ class RiverWaterLevelData(
     @cached_property
     def station(self):
         return Station.from_name(self.station_name)
+
+    @cached_property
+    def alert(self):  # noqa: CFQ004
+        if self.water_level_m >= self.station.major_flood_level_m:
+            return Alert.MAJOR
+        if self.water_level_m >= self.station.minor_flood_level_m:
+            return Alert.MINOR
+        if self.water_level_m >= self.station.alert_level_m:
+            return Alert.ALERT
+        return Alert.NORMAL
