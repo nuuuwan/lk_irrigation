68f8a5b2a0f763c809999994efbf2f264a2bc4c2
nuuuwan <nuuuwan@gmail.com>
2025-12-04 09:17:49 +0530

    Add write_alert_data_list method to handle alert data serialization

--- src/lk_irrigation/rwld/RiverWaterLevelDataFileWriteMixin.py
+++ src/lk_irrigation/rwld/RiverWaterLevelDataFileWriteMixin.py
@@ -7,6 +7,10 @@ log = Log("RiverWaterLevelDataFileWriteMixin")
 
 
 class RiverWaterLevelDataFileWriteMixin:
+    URL_STRUCTURED = (
+        "https://raw.githubusercontent.com"
+        + "/nuuuwan/lk_irrigation/refs/heads/main/data/all.json"
+    )
 
     def write(self):
         if self.json_file.exists:
@@ -15,6 +19,27 @@ class RiverWaterLevelDataFileWriteMixin:
         self.json_file.write(asdict(self))
         log.info(f"Wrote {self.json_file}")
         return True
+
+    @classmethod
+    def write_alert_data_list(cls, d_list):
+        json_file = JSONFile(
+            os.path.join(cls.DIR_DATA, "alert_data_list.json")
+        )
+
+        def mapper(d):
+            return {
+                "id": d["station_name"],
+                "time_ut": d["time_ut"],
+                "value_idx": {
+                    "water_level_m": d["water_level_m"],
+                },
+                "url_source": cls.REMOTE_URL,
+                "url_structured": cls.URL_STRUCTURED,
+            }
+
+        alert_data_list = [mapper(d) for d in d_list]
+        json_file.write(alert_data_list)
+        log.info(f"Wrote {json_file}")
 
     @classmethod
     def write_all(cls):
@@ -30,3 +55,6 @@ class RiverWaterLevelDataFileWriteMixin:
         cls.station_to_latest.cache_clear()
         cls.station_to_ror.cache_clear()
         log.debug("Cleared caches for list_all and related methods.")
+
+        # alert_data_list
+        cls.write_alert_data_list(d_list)
